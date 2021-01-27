b096e0032feb7b604b42a19c9bf7b1a829fcda4c
Manaragabriel <gabrielmanara2010@hotmail.com>
2021-01-27 20:58:23 -0300

    Continuing site construction

--- resources/views/site/service_site.blade.php
+++ resources/views/site/service_site.blade.php
@@ -38,6 +38,65 @@
 
 </section>
 
+<section class="p-5">
+    <div class="container">
+        <h2 class="font-weight-bold text-center  h2">Vantagens</h2>
 
+        <div class="row py-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                <div class="service-block-three__single">
+                    <div class="icon-box"><span class="flaticon-responsive"></span></div>
+                    <h6><a href="web-development.html">Mobile <br>First</a></h6>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-4 wow fadeInLeft" data-wow-delay="600ms" data-wow-duration="1500ms">
+                <div class="service-block-three__single">
+                    <div class="icon-box"><span class="flaticon-development"></span></div>
+                    <h6><a href="digital-marketing.html">Painel <br>Intuitivo</a></h6>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-4 wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
+                <div class="service-block-three__single">
+                    <div class="icon-box"><span class="flaticon-app-development"></span></div>
+                    <h6><a href="app-development.html">SEO <br> Friendly</a></h6>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-4 wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
+                <div class="service-block-three__single">
+                    <div class="icon-box"><span class="flaticon-app-development"></span></div>
+                    <h6><a href="app-development.html">Plataforma <br> Própria</a></h6>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-4 wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
+                <div class="service-block-three__single">
+                    <div class="icon-box"><span class="flaticon-app-development"></span></div>
+                    <h6><a href="app-development.html">CMS<br></a></h6>
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-md-6 col-lg-4 wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
+                <div class="service-block-three__single">
+                    <div class="icon-box"><span class="flaticon-app-development"></span></div>
+                    <h6><a href="app-development.html">Segurança <br></a></h6>
+                </div>
+            </div>
+
+    </div>
+
+
+</section>
+
+<section id="contato" class="p-5">
+    <div class="container">
+        <h4 class="text-center fadeInLeft">Venha falar com a gente!</h4>
+        <livewire:site.contact-form>
+            </livewire>
+
+    </div>
+</section>
 
 @endsection
